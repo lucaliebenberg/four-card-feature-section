1e1279e76688e9df6fe730bae24884dc6882a5eb
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-02-27 14:37:13 +0200

    Heading added to Desktop viewport

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,14 @@ import Card from "./components/Card";
 const App = () => {
   return (
     <div className="container">
-      <Card />
+      {/* <div className="title__wrapper"> */}
+      <h1 className="container_title1">Reliable, efficient delivery</h1>
+      <h1 className="container_title2">Powered by Technology</h1>
+      <p className="container_para">
+        Our Artificial Intelligence powered tools use millions of project data
+        points to ensure that your project is successful
+      </p>
+      {/* </div> */}
     </div>
   );
 };
